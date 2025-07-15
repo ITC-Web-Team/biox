676daf56e5ca49bf6c0a32fc30f5372060724a77
Nitansh <24b1807@iitb.ac.in>
2025-07-15 17:34:04 +0530

    set up backend

---
+++ biox-frontend/src/components/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Hero.css'
+
+function Hero() {
+  return (
+    <div className='home-section'>
+        <div id='left-section'>
+            <p className='big-text'>BioX Club<br/>IIT Bombay</p>
+            <p className='small-text'>The BioX Club at IIT Bombay was founded in 2015 under the Institute Technical Council to bring together students passionate about Biotechnology and Interdisciplinary Biology. Despite belonging to different disciplines and years, many students share a vision of contributing to India's future through research and innovation in this rapidly evolving field. </p>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
