9c0ecf0378d9e61dfed8eea4c39cad69ff07111e
Tezas <tezas9999@gmail.com>
2025-10-01 16:45:02 +0530

    home page imgs

--- biox-frontend/src/components/Abouthp.jsx
+++ biox-frontend/src/components/Abouthp.jsx
@@ -11,15 +11,25 @@ function Abouthp() {
                 <p className = "about-para">We are BioX, a student club at IIT Bombay that unites the fields of engineering and biology. We are a community who enjoy investigating the fascinating connection between biology, tech, and innovation—all while maintaining an engaging and enjoyable environment.</p>
                 <button className = "filled-button">THE TEAM <LiaExternalLinkSquareAltSolid /></button>
             </div>
-            <div className = "about-image"></div>
+            <div className = "about-image">
+                <img src='./home-images/brain.JPG' alt='brain'>
+                </img>
+            </div>
         </div>
 
         <div className = "block">
-          <div className = "about-image"></div>
+          <div className = "about-image">
+                <img src='./home-images/dna.JPG' alt='dna'>
+                </img>
+          </div>
             <div className = "about-text">
                 <h1 className = "about-heading">our purpose?</h1>
                 <p className = "about-para">Our purpose is to make biotechnology accessible, engaging, and collaborative. Through talks, workshops, projects, and events, we create a platform where students from every background can dive into the life sciences and discover how it connects with engineering and technology.</p>
             </div>
+            {/* <div className = "about-image">
+                <img src='./home-images/dna.JPG' alt='dna'>
+                </img>
+            </div> */}
         </div>
 
         <div className = "block">
@@ -27,7 +37,9 @@ function Abouthp() {
                 <h1 className = "about-heading">our vision?</h1>
                 <p className = "about-para">Our vision is a community of bio-enthusiast engineers. By fostering curiosity and collaboration, we aim to nurture the next generation of innovators who will push the boundaries of biotechnology.</p>
             </div>
-            <div className = "about-image"></div>
+            <div className = "about-image">
+                <img src='./home-images/microscope.JPG' alt='microscope'></img>
+            </div>
         </div>
     </div>
   )
